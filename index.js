113c8ee06b19772ffae648129dc6615ec2bd5b17
Eric Bandera <eric.bandera@ced.com>
2022-10-31 12:46:41 -0500

    updated styling and customizations

--- index.js
+++ index.js
@@ -24,12 +24,26 @@ function App(){
     temp.splice(index, 1);
     setTodos(temp);
   }
+  const hoverItem = (text,index,xpos,ypos) => {
+   const el= document.createElement('div');
+   el.innerText = "Remove " + text;
+   el.className = "hoverBox";
+   el.style.left = xpos + "px";
+   el.style.top = ypos + "px";
+   el.id = "hover" + index;
+   document.getElementById('root').appendChild(el);
+}
+  const unhoverItem = id =>
+  {
+    const el =document.getElementById(id);
+    document.getElementById('root').removeChild(el);
+  }
 
   return( 
     <div className="app">
         <div className="todo-list">
           {todos.map((todo, i) => 
-            <Todo index={i} key={i} todo={todo} remove={removeTodo}/>)}
+            <Todo index={i} key={i} todo={todo} remove={removeTodo} hoverEffectsOver={hoverItem} hoverEffectsOut={unhoverItem}/>)}
         
           <TodoForm addTodo={addTodo}/> 
         </div>
--- todo.js
+++ todo.js
@@ -1,4 +1,16 @@
-function Todo({todo,index, remove}) {
+function Todo({todo,index, remove, hoverEffectsOver,hoverEffectsOut}) {
     const handle = () => remove(index);
-    return  <div className="todo" key={index} id={index} onClick={handle}>{todo.text} (-)</div>
+    const handleOver = e => {
+        const el = document.getElementById(index);
+        const rect= el.getBoundingClientRect();
+        const x = rect.left + window.scrollX +rect.width -20;
+        const y = rect.top + window.scrollY +5;
+     
+        hoverEffectsOver(todo.text,index,x,y);
+    }
+    const handleOut=() => {
+        hoverEffectsOut("hover" + index)
+    }
+
+    return  <div className="todo" key={index} id={index} onClick={handle} onMouseOver={handleOver} onMouseOut={handleOut}>{todo.text} (-)</div>
 }
